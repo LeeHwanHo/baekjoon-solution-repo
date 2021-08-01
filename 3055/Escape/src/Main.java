0fc63123ba5052ac78328daf4d15e3d2eb72f25e
Lee Hwan Ho <43461392+LeeHwanHo@users.noreply.github.com>
2021-08-01 11:30:41 +0900

    3055 탈출 문제 성공 commit - 코드 리팩토링 필요

--- 3055/Escape/src/Main.java
+++ 3055/Escape/src/Main.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -9,81 +7,186 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = null;
-        int[] dColumn = {-1,0,0,1};
-        int[] dRow = {0,-1,1,0};
+        StringTokenizer st = null;
+        Node beaverNode = null;
 
-        //row,column 입력
+        // row,column 입력
         st = new StringTokenizer(br.readLine());
-        int row = Integer.parseInt(st.nextToken());
-        int column = Integer.parseInt(st.nextToken());
-            
-        Node[][] map = InitMap(row,column);
+        int rowCnt = Integer.parseInt(st.nextToken());
+        int columnCnt = Integer.parseInt(st.nextToken());
 
+        Node[][] map = InitMap(rowCnt, columnCnt);
 
+        beaverNode = SetMap(br, map, rowCnt, columnCnt);
+        if (beaverNode == null) {
+            System.out.println("input error : beaver Node is not exist");
+            return;
+        }
+        int cnt = Search(map, rowCnt, columnCnt, beaverNode);
 
-        if(br != null) br.close();
-        if(bw != null) bw.close();
+        if (cnt == -1) {
+            System.out.println("KAKTUS");
+        } else {
+            System.out.println(cnt);
+        }
+        br.close();
     }
 
-    static class Node{
-        public int row,column;
-        public boolean marked;
+    static class Node {
+        public int row, column, depth;
+        public boolean beaverMarked, imWater, imWaterTobe, imStone, imTarget;
         public LinkedList<Node> adjacent = null;
-        public boolean imWater;
-        public int depth;
 
-        Node(int row,int column){
+        Node(int row, int column) {
             this.row = row;
             this.column = column;
             adjacent = new LinkedList<Node>();
-            marked = false;
+            beaverMarked = false;
             imWater = false;
+            imWaterTobe = false;
+            imStone = false;
+            imTarget = false;
             depth = 0;
         }
     }
 
-
     // BFS
-    static public void minute(int rowCnt,int columnCnt,Node S,Node water,Node D){
-        Queue<Node> queueS = new LinkedList<Node>();
-        Queue<Node> queueWater = new LinkedList<Node>();
-        S.marked = true;
-        water.marked = true;
+    static public int Search(Node[][] map, int rowCnt, int columnCnt, Node beaverNode) throws Exception {
+        // Node S,Node[] waters,Node D
+        Node S = beaverNode;
 
-        while(!queueS.isEmpty()){
-            Node curSNode = queueS.poll();
-            Node curWaterNode = queueWater.poll();
-            if(!queueWater.isEmpty()){
-                for(Node node : curWaterNode.adjacent){
-                    if(!node.marked){
-                        node.marked = true;
-                        node.imWater = true;
-                        queueWater.add(node);
+        Queue<Node> queueS = new LinkedList<Node>();
+        queueS.add(S);
+
+        S.beaverMarked = true;
+
+        while (!queueS.isEmpty()) {
+            // waters Move
+            for (int row = 0; row < rowCnt; row++) {
+                for (int column = 0; column < columnCnt; column++) {
+                    Node node = map[row][column];
+                    if (node.imWater) {
+                        for (Node adjacentNode : node.adjacent) {
+                            if (!adjacentNode.imStone && !adjacentNode.imTarget) {
+                                adjacentNode.imWaterTobe = true;
+                            }
+                        }
                     }
                 }
             }
-            for(Node node : curSNode.adjacent){
-                if(!node.marked && !node.imWater){
-                    node.marked = true;
-                    node.depth = curSNode.depth + 1;
-                    queueS.add(node);
+
+            // WaterToboe To Water
+            for (int row = 0; row < rowCnt; row++) {
+                for (int column = 0; column < columnCnt; column++) {
+                    Node node = map[row][column];
+                    if (node.imWaterTobe) {
+                        node.imWater = true;
+                    }
                 }
             }
-        }
+            while(queueS.peek() != null){
+                Node curSNode = queueS.poll();
+                if (curSNode.imTarget) { return curSNode.depth;}
+                
+                // Check Beaver
+                for (Node node : curSNode.adjacent) {
+                    if (!node.beaverMarked && !node.imWater && !node.imStone) {
+                        node.beaverMarked = true;
+                        node.depth = curSNode.depth + 1;
+                        queueS.add(node);
+                    }
+                }
+                
+                if(queueS.peek() == null) break;
+                if(queueS.peek().depth != curSNode.depth) break;
+            }
 
+            // debuggingDraw(map,rowCnt,columnCnt);
+        }
+        return -1;
     }
 
-    static public Node[][] InitMap(int rowCnt,int columnCnt){
+    // map 초기화
+    static public Node[][] InitMap(int rowCnt, int columnCnt) {
         Node[][] map = new Node[rowCnt][columnCnt];
 
-        for(int row = 0;row < rowCnt;row++){
-            for(int column = 0;column < columnCnt;column++){
-                map[row][column] = new Node(row,column);
+        for (int row = 0; row < rowCnt; row++) {
+            for (int column = 0; column < columnCnt; column++) {
+                map[row][column] = new Node(row, column);
             }
         }
 
         return map;
     }
+
+    // map 입력 및 인접노드 체크
+    static public Node SetMap(BufferedReader br, Node[][] map, int rowCnt, int columnCnt) throws Exception {
+        Node beaverNode = null;
+        for (int row = 0; row < rowCnt; row++) {
+            String[] value = br.readLine().split("");
+            for (int column = 0; column < columnCnt; column++) {
+                Node node = map[row][column];
+                switch (value[column]) {
+                    case "D":
+                        node.imTarget = true;
+                        break;
+                    case "X":
+                        node.imStone = true;
+                        break;
+                    case "*":
+                        node.imWater = true;
+                        break;
+                    case "S":
+                        beaverNode = node;
+                        break;
+                }
+
+                if (!node.imTarget && !node.imStone) {
+                    // left
+                    int chkColumn = node.column - 1;
+                    if (chkColumn >= 0) {
+                        node.adjacent.add(map[row][chkColumn]);
+                    }
+                    // upper
+                    int chkRow = node.row - 1;
+                    if (chkRow >= 0) {
+                        node.adjacent.add(map[chkRow][column]);
+                    }
+                    // right
+                    chkColumn = node.column + 1;
+                    if (chkColumn >= 0 && chkColumn < columnCnt) {
+                        node.adjacent.add(map[row][chkColumn]);
+                    }
+                    // down
+                    chkRow = node.row + 1;
+                    if (chkRow >= 0 && chkRow < rowCnt) {
+                        node.adjacent.add(map[chkRow][column]);
+                    }
+                }
+            }
+        }
+
+        return beaverNode;
+    }
+
+    // static void debuggingDraw(Node[][] map,int r,int c) throws Exception{
+    //     for(int row =0;row < r;row++){
+    //         System.out.println();
+    //         for(int column = 0; column < c;column++){
+    //             Node node = map[row][column];
+    //             String pic = "";
+    //             if(node.imStone) {pic="X";}
+    //             if(node.imTarget) {pic="D";}
+    //             if(node.imWater) {pic="*";}
+    //             if(node.imWaterTobe && !node.imWater) {pic="⁂";}
+    //             if(node.beaverMarked && !node.imWater) {pic="S";}
+
+    //             if(pic == ""){pic=".";}
+    //             System.out.print(pic);
+    //         }
+    //     }
+    //     System.out.println();
+    //     System.out.println("====================================================== Next Move ======================================================");
+    //     Thread.sleep(2000);
+    // }
 }
