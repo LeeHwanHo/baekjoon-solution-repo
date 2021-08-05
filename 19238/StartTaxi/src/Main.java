842b600c24d1af393f87d95f0e9b86b9d6a25c00
Lee Hwan Ho <43461392+LeeHwanHo@users.noreply.github.com>
2021-08-06 00:00:44 +0900

    Start Taxi 진행 Commit ASIS  - InitMap    - Map 크기만큼 Node 할당  - SetNode    - Wall Node 세팅    - Adjacent Node 세팅  - SetObject    - taxi Node, passenger,destination Node 세팅 TODO - FindMinDistPassenger 함수 구현

--- 19238/StartTaxi/src/Main.java
+++ 19238/StartTaxi/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -17,13 +18,16 @@ public class Main {
 
         map = InitMap(N);
         SetNode(st, br, map);
+        Node startNode = SetObject(st, br, map, M);
+
 
         if(br != null) br.close();
     }
 
     static public class Node{
         public int row,column;
-        public boolean marked,imWall;
+        public boolean marked,imWall,imPassenger,imDest;
+        public int number;
         public LinkedList<Node> adjacent = null;
 
         Node(int row,int column){
@@ -31,6 +35,8 @@ public class Main {
             this.column = column;
             marked = false;
             imWall = false;
+            imPassenger = false;
+            imDest = false;
 
             adjacent = new LinkedList<Node>();
         }
@@ -57,5 +63,72 @@ public class Main {
                 if(Integer.parseInt(st.nextToken().trim()) == 1) node.imWall = true;
             }
         }
+
+        for(int row = 0;row < map.length;row++){
+            for(int column = 0;column < map[row].length;column++){
+                Node node = map[row][column];
+                int nextColumn = column - 1;
+                if(nextColumn >= 0 && nextColumn < map[row].length){
+                    Node adjacentNode = map[row][nextColumn];
+                    if(!adjacentNode.imWall)
+                        node.adjacent.add(adjacentNode);
+                }
+                int nextRow = row + 1;
+                if(nextRow < map.length){
+                    Node adjacentNode = map[nextRow][column];
+                    if(!adjacentNode.imWall)
+                        node.adjacent.add(adjacentNode);
+                }
+                nextColumn = column + 1;
+                if(nextColumn < map[row].length){
+                    Node adjacentNode = map[row][nextColumn];
+                    if(!adjacentNode.imWall)
+                        node.adjacent.add(adjacentNode);
+                }
+                nextRow = row - 1;
+                if(nextRow >= 0 && nextRow < map.length){
+                    Node adjacentNode = map[nextRow][column];
+                    if(!adjacentNode.imWall)
+                        node.adjacent.add(adjacentNode);
+                }
+            }
+        }
+    }
+
+    static public Node SetObject(StringTokenizer st,BufferedReader br,Node[][] map,int M) throws Exception{
+        st = new StringTokenizer(br.readLine());
+        int startRow = Integer.parseInt(st.nextToken().trim()) - 1;
+        int startColumn = Integer.parseInt(st.nextToken().trim()) - 1;
+        Node taxiNode = map[startRow][startColumn];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0;i < M;i++){
+            st = new StringTokenizer(br.readLine());
+            int passengerRow = Integer.parseInt(st.nextToken().trim()) - 1;
+            int passengerColumn = Integer.parseInt(st.nextToken().trim()) - 1;
+            int destRow = Integer.parseInt(st.nextToken().trim()) - 1;
+            int destColumn = Integer.parseInt(st.nextToken().trim()) - 1;
+
+            Node passengerNode = map[passengerRow][passengerColumn];
+            Node destNode = map[destRow][destColumn];
+            passengerNode.imPassenger = true;
+            passengerNode.number = i;
+            destNode.imDest = true;
+            destNode.number = i;
+        }
+
+        return taxiNode;
+    }
+
+    static public Node FindMinDistPassenger(Node startNode,Node[][] map){
+        Node targetPassengerNode;
+        Node[][] cloneMap = map.clone();
+
+        /*TODO
+        * 택시로부터 거리가 가장 짧은 승객을 찾아 리턴해주는 함수 구현 (BFS)
+        * 찾았을 경우 original map에 반영 *구태여 두번 계산할 필요 없으니..
+        */
+
+        return targetPassengerNode;
     }
 }
